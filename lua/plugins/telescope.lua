491a1b03d9a9eeb7ba401902826fcbd09dd7e367
Spoeperd <simon.verdonk@icloud.com>
2026-05-17 02:36:29 +0200

    telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,8 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Telescope: find files" },
-		{ "<C-p>",      "<cmd>Telescope git_files<cr>",  desc = "Telescope: git files" },
-        { "<leader>ps", "<cmd>Telescope live_grep<cr>", desc = "Telescope: live grep" },
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Telescope: find files" },
+        { "<leader>ps", "<cmd>Telescope live_grep<cr>",  desc = "Telescope: live grep" },
     },
 }
