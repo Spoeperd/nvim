7dc9f89917862efc445dcfeddc2fcdb41eca4e8b
Spoeperd <simon.verdonk@icloud.com>
2026-05-24 01:50:53 +0200

    reformat

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         formatters_by_ft = {
             python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
-            lua = { "stylelua" },
+            lua = { "stylua" },
         },
         format_on_save = {
             timeout_ms = 500,
