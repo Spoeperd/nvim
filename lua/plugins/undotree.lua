5108638c4748d39527a1fc45d3d7610791cac17b
Spoeperd <simon.verdonk@icloud.com>
2026-05-24 01:12:11 +0200

    nothing

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,9 +1,9 @@
 return {
-	"mbbill/undotree",
-	keys = {
-		{ "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
-	},
+    "mbbill/undotree",
     init = function()
         vim.g.undotree_SetFocusWhenToggle = 1
     end,
+    keys = {
+        { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+    },
 }
